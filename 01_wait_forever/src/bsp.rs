f7f1bd4e9f1793e4ebbb506cefa0a51247f357d7
晒晕的兔子 <gubu1986@126.com>
2021-11-09 20:17:04 +0800

    support k210

--- 01_wait_forever/src/bsp.rs
+++ 01_wait_forever/src/bsp.rs
@@ -9,3 +9,9 @@ mod raspberrypi;
 
 #[cfg(any(feature = "bsp_rpi3", feature = "bsp_rpi4"))]
 pub use raspberrypi::*;
+
+#[cfg(any(feature = "bsp_k210"))]
+mod k210;
+
+#[cfg(any(feature = "bsp_k210"))]
+pub use k210::*;
