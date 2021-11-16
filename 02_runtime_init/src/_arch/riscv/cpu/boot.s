efd1830ea8acb92bf4d8e45a2ba3d08c33254849
buhe <bugu1986@126.com>
2021-11-16 10:51:08 +0800

    k210 support

--- 02_runtime_init/src/_arch/riscv/cpu/boot.s
+++ 02_runtime_init/src/_arch/riscv/cpu/boot.s
@@ -5,16 +5,38 @@
 //--------------------------------------------------------------------------------------------------
 // Public Code
 //--------------------------------------------------------------------------------------------------
+.equ _core_id_mask, 0b11
+
 .section .text._start
 .globl	_start
 //------------------------------------------------------------------------------
 // fn _start()
 //------------------------------------------------------------------------------
 _start:
-	la sp , __boot_core_stack_end_exclusive
-	j _start_rust
+	csrr x5, mhartid
+	andi	x5, x5, _core_id_mask
+	la	x6, BOOT_CORE_ID
+	bne	x5, x6, .L_parking_loop
+
+	la	x5, __bss_start
+	la x6, __bss_end_exclusive
 	// Infinitely wait for events (aka "park the core").
-L_parking_loop:
+.L_bss_init_loop:
+	# cmp	x0, x1
+	beq	x5, x6, .L_prepare_rust
+	# stp	x0, x0, [x5], #16
+	j	.L_bss_init_loop
+
+	// Prepare the jump to Rust code.
+.L_prepare_rust:
+	// Set the stack pointer.
+	la sp, __boot_core_stack_end_exclusive
+
+	// Jump to Rust code.
+	j	_start_rust
+
+	// Infinitely wait for events (aka "park the core").
+.L_parking_loop:
 	wfi
-	j	L_parking_loop
+	j	.L_parking_loop
 
