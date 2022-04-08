f3c8a0305b1e3614882eb9a4c194234fc34d13b9
buhe <bugu1986@126.com>
2022-04-08 19:48:56 +0800

    u

--- 02_runtime_init/src/_arch/riscv/cpu/boot.s
+++ 02_runtime_init/src/_arch/riscv/cpu/boot.s
@@ -5,7 +5,7 @@
 //--------------------------------------------------------------------------------------------------
 // Public Code
 //--------------------------------------------------------------------------------------------------
-.equ _core_id_mask, 0b01
+.equ _core_id_mask, 0b11
 
 .section .text._start
 .globl	_start
@@ -15,7 +15,7 @@
 _start:
 	csrr x5, mhartid
 	andi	x5, x5, _core_id_mask
-	la	x6, BOOT_CORE_ID
+	li	x6, 0
 	bne	x5, x6, .L_parking_loop
 
 	la	x5, __bss_start
@@ -24,7 +24,7 @@ _start:
 .L_bss_init_loop:
 	# cmp	x0, x1
 	beq	x5, x6, .L_prepare_rust
-	# stp	x0, x0, [x5], #16
+	# stp	x0, x0, [x5], #16  # todo
 	j	.L_bss_init_loop
 
 	// Prepare the jump to Rust code.
