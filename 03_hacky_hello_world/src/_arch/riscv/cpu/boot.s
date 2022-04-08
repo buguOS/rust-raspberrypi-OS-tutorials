22179c6b9312cd90b7080afbd68b8193033d4b47
buhe <bugu1986@126.com>
2022-04-08 20:00:57 +0800

    u

--- 03_hacky_hello_world/src/_arch/riscv/cpu/boot.s
+++ 03_hacky_hello_world/src/_arch/riscv/cpu/boot.s
@@ -6,17 +6,16 @@
 // Public Code
 //--------------------------------------------------------------------------------------------------
 .equ _core_id_mask, 0b11
+
 .section .text._start
 .globl	_start
 //------------------------------------------------------------------------------
 // fn _start()
 //------------------------------------------------------------------------------
 _start:
-	//la sp , __boot_core_stack_end_exclusive
-	//j _start_rust
 	csrr x5, mhartid
 	andi	x5, x5, _core_id_mask
-	la	x6, BOOT_CORE_ID
+	li	x6, 0
 	bne	x5, x6, .L_parking_loop
 
 	la	x5, __bss_start
@@ -25,7 +24,7 @@ _start:
 .L_bss_init_loop:
 	# cmp	x0, x1
 	beq	x5, x6, .L_prepare_rust
-	# stp	x0, x0, [x5], #16
+	# stp	x0, x0, [x5], #16  # todo
 	j	.L_bss_init_loop
 
 	// Prepare the jump to Rust code.
@@ -35,8 +34,8 @@ _start:
 
 	// Jump to Rust code.
 	j	_start_rust
-	// Infinitely wait for events (aka "park the core").
 
+	// Infinitely wait for events (aka "park the core").
 .L_parking_loop:
 	wfi
 	j	.L_parking_loop
